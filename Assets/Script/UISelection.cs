e1c49e8d807c9d1eec493d153c90c0e338d1cc9d
VaEUO <valeria.urrutia@ingenieria.unam.edu>
2026-03-02 12:53:53 -0600

    clase0203

--- Assets/Script/UISelection.cs
+++ Assets/Script/UISelection.cs
@@ -1,16 +1,78 @@
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using System.Collections;
+using System.Collections.Generic;
+using System;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+
+public class UISelection : MonoBehaviour
 {
+    public static bool gazedAt;
+    public float fillTime = 5f;
+    public Image radialImage;
+    public UnityEvent onFillComplete;
+
+    private Coroutine fillCoroutine;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        gazedAt = false;
+        radialImage.fillAmount = 0;
+
     }
 
+    public void OnPointerEnter()
+    {
+        gazedAt = true;
+
+        if (fillCoroutine != null)
+        {
+            StopCoroutine(fillCoroutine);
+
+        }
+        fillCoroutine = StartCoroutine(FillRadial());
+
+    }
+    public void OnPointerExit()
+    {
+        gazedAt = false;
+
+        if (fillCoroutine != null)
+        {
+            StopCoroutine(fillCoroutine); //Detine el llenado
+            fillCoroutine = null;
+
+        }
+        radialImage.fillAmount = 0f; // Reinciae lllenado a 0
+    }
+    private IEnumerator FillRadial()
+    {
+        float elapsedTime = 0f;
+
+        while (elapsedTime < fillTime)
+        {
+            if(!gazedAt) //Deja de ver el boton
+            {
+                yield break;
+            }
+
+            // elpsedTime = elapsedTime + TIme.deltaTime
+            elapsedTime += Time.deltaTime;
+            radialImage.fillAmount = Mathf.Clamp01(elapsedTime/fillTime);
+
+            yield return null;
+        }
+
+        onFillComplete?.Invoke();
+
+        //Console.WriteLine(gazedAt?"Verdadero":"Falso")
+    }
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
+
+
